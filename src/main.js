739b78024d2244c3077743f41d2c3ea89d4231a8
mamiyaso <mmyaso@tutanota.com>
2024-08-10 13:08:54 +0300

    fix deployment issues

--- src/main.js
+++ src/main.js
@@ -1,19 +1,8 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import Home from './views/Home.vue'
-import AddBook from './views/AddBook.vue'
-import BookDetails from './views/BookDetails.vue'
+import router from './router'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: Home },
-    { path: '/add', component: AddBook },
-    { path: '/book/:id', component: BookDetails, props: true }
-  ]
-})
 
 createApp(App).use(router).mount('#app')
