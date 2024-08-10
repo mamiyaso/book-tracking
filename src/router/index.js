bdabdd971c7df6863a5ba413a8cff14790e186f1
mamiyaso <mmyaso@tutanota.com>
2024-08-10 12:41:05 +0300

    fix to github pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 import Home from '../views/Home.vue'
 import AddBook from '../views/AddBook.vue'
 import BookDetails from '../views/BookDetails.vue'
 
 const router = createRouter({
-  history: createWebHashHistory('/book-tracking/'),
+  history: createWebHashHistory (),
   routes: [
     { 
-      path: '/',
+      path: '/book-tracking',
       name: 'home',
       component: Home 
     },
     { 
-      path: '/add',
+      path: '/book-tracking/add',
       name: 'add',
       component: AddBook 
     },
     { 
-      path: '/book/:id',
+      path: '/book-tracking/book/:id',
       name: 'bookDetails',
       component: BookDetails,
       props: true 
